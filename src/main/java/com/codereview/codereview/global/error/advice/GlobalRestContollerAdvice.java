0d8c9a31ce23d8a48b772807d12e539b1eb57270
Domae-back-end <ty_ty123@naver.com>
2023-10-28 13:01:48 +0900

    feat : 로그인 기능 추가

--- src/main/java/com/codereview/codereview/global/error/advice/GlobalRestContollerAdvice.java
+++ src/main/java/com/codereview/codereview/global/error/advice/GlobalRestContollerAdvice.java
@@ -1,5 +1,6 @@
 package com.codereview.codereview.global.error.advice;
 
+import com.codereview.codereview.global.error.exception.LoginExceptionImpl;
 import com.codereview.codereview.global.error.exception.RegisterExceptionImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,11 @@ public class GlobalRestContollerAdvice {
 
     @ExceptionHandler(RegisterExceptionImpl.class)
     public ResponseEntity<?> registerException(RegisterExceptionImpl e) {
+        return ResponseEntity.status(e.getErrorType().getErrorCode().statusCode())
+                .body(e.getErrorType().getErrorCode());
+    }
+    @ExceptionHandler(LoginExceptionImpl.class)
+    public ResponseEntity<?> loginException(LoginExceptionImpl e) {
         return ResponseEntity.status(e.getErrorType().getErrorCode().statusCode())
                 .body(e.getErrorType().getErrorCode());
     }
