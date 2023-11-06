02a601d86a5afae7d6faf46d9207c1d04065293b
Domae-back-end <ty_ty123@naver.com>
2023-11-06 21:24:53 +0900

    feat : CI/CD EC2 배포 구축

--- src/main/java/com/codereview/codereview/global/config/WebSecurityConfig.java
+++ src/main/java/com/codereview/codereview/global/config/WebSecurityConfig.java
@@ -65,7 +65,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
+
         http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()));
+
         http.sessionManagement((sessionManagement) ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
